97c891aa8c7a2164d133609484ba5f1f6803ec57
LeeSangJin92 <151722889+LeeSangJin92@users.noreply.github.com>
2023-12-11 16:29:02 +0900

    default commit

---
+++ src/main/java/com/example/withbuddy_project/WithbuddyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.withbuddy_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithbuddyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithbuddyProjectApplication.class, args);
+	}
+
+}
