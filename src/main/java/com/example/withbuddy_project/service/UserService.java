ce30501de273e7a6ec57689317643b1e649b0c5a
ChoiKangHyun <chlhun416@gmail.com>
2023-12-21 14:44:44 +0900

    hyun 2023-12-21 14:44 merge 전 테스트

--- src/main/java/com/example/withbuddy_project/service/UserService.java
+++ src/main/java/com/example/withbuddy_project/service/UserService.java
@@ -1,4 +1,22 @@
 package com.example.withbuddy_project.service;
 
+import com.example.withbuddy_project.domain.Authority;
+import com.example.withbuddy_project.domain.User;
+import com.example.withbuddy_project.domain.dto.BuddyDto;
+import com.example.withbuddy_project.domain.dto.UserDto;
+
+import java.util.List;
+
 public interface UserService {
+    List<Authority> selectAuthoritiesById(Long id);
+
+    User findByUsername(String userId);
+
+    List<UserDto> findWithoutMe(Long id);
+
+    User findById(Long id);
+
+    BuddyDto findBuddy(Long id, Long code);
+
+    List<User> findDm(Long loginId);
 }
--- src/main/java/com/example/withbuddy_project/service/UserServiceImpl.java
+++ src/main/java/com/example/withbuddy_project/service/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.example.withbuddy_project.service;
 
+import com.example.withbuddy_project.domain.Authority;
 import com.example.withbuddy_project.domain.User;
 import com.example.withbuddy_project.domain.dto.AddressUserDto;
 import com.example.withbuddy_project.domain.dto.BuddyDto;
 import com.example.withbuddy_project.domain.dto.UserDto;
+import com.example.withbuddy_project.repository.AuthorityRepository;
 import com.example.withbuddy_project.repository.UserRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.ibatis.session.SqlSession;
@@ -13,12 +15,14 @@ import java.util.List;
 
 @Service
 @Slf4j
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService {
 
     private UserRepository userRepository;
+    private AuthorityRepository authorityRepository;
 
     public UserServiceImpl(SqlSession sqlSession) {
         userRepository = sqlSession.getMapper(UserRepository.class);
+        authorityRepository = sqlSession.getMapper(AuthorityRepository.class);
     }
 
 
@@ -26,24 +30,34 @@ public class UserServiceImpl {
         return userRepository.findByUsername(userId);
     }
 
-
+    @Override
     public List<UserDto> findWithoutMe(Long id) { // 나 뺴고 찾음
         return userRepository.findAllWithoutId(id);
     }
 
 
+    @Override
     public User findById(Long id) {
         return userRepository.findById(id);
     }
 
+    @Override
     public BuddyDto findBuddy(Long id,Long code) {
         return userRepository.findUserById(id,code);
     }
 
+    @Override
     public List<User> findDm(Long loginId) { // 로그인한 유저가 갖고있는 모든 dm채팅방 정보
 
         List<User> list = userRepository.findDmListByLoginUserId(loginId);
         log.info("dmList user: {} ", list);
         return list;
     }
+
+    @Override
+    public List<Authority> selectAuthoritiesById(Long id) {
+        User user = userRepository.findById(id);
+        return authorityRepository.findByUser(user);
+    }
+
 }
