c8bde373c5a9d1ce3531178ce0540998a8a787c4
ChoiKangHyun <chlhun416@gmail.com>
2023-12-26 11:22:57 +0900

    hyun 2023-12-26 11:22  수정

--- src/main/java/com/lec/spring/withbuddy_project/controller/UserApiController.java
+++ src/main/java/com/lec/spring/withbuddy_project/controller/UserApiController.java
@@ -6,14 +6,11 @@ import com.lec.spring.withbuddy_project.domain.dto.UserDto;
 import com.lec.spring.withbuddy_project.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class UserApiController {
 
